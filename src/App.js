86d2542e3511a9a261825a0904af8add71e74122
g-rmc <g.rmc3000@gmail.com>
2022-08-02 16:37:31 -0300

    Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
-function App() {
+import UserContext from "./contexts/UserContext";
+
+export default function App() {
+
+  const [tasks, setTasks] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{tasks, setTasks}}>
+
+      Oi :D
+      
+    </UserContext.Provider>
   );
 }
-
-export default App;
